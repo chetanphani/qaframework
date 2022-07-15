1b6c5f3d0ee2b7afad91fe310733f103752b23c2
chetan <chetan@192.168.29.143>
2022-07-15 19:06:39 +0530

    one

---
+++ fake2/src/main/java/FAKE2/fake2/App.java
@@ -0,0 +1,13 @@
+package FAKE2.fake2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
